1cfe47800e8af2162b851d886e52481896d24f6d
Lila Dangoriya <71085463+javicmusk@users.noreply.github.com>
2026-01-31 01:38:50 +0545

    Refactor URL patterns for token authentication; add names to token routes

--- mindtrack/mindtrack/urls.py
+++ mindtrack/mindtrack/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -24,9 +23,9 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
 
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/', include('core.urls')),  # keep your app urls
 ]
-
